2d93874a69cc24c06d1a1f933ca8d72e77232498
imromanda <imromanda@gmail.com>
2026-05-20 21:13:03 +0200

    refactor: remove unused ConstraintLayout attributes from XML layout

--- apps/showcase-projects/1-5-1-ui-demo-accessible/app/src/main/java/com/example/ui_demo_accessible/MainActivity.java
+++ apps/showcase-projects/1-5-1-ui-demo-accessible/app/src/main/java/com/example/ui_demo_accessible/MainActivity.java
@@ -53,7 +53,10 @@ public class MainActivity extends AppCompatActivity {
           public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
           @Override
-          public void onTextChanged(CharSequence s, int start, int before, int count) {}
+          public void onTextChanged(CharSequence s, int start, int before, int count) {
+            // actualiza el textOverlay mientras onTextChanged
+            binding.textOverlay.setText(s.toString());
+          }
 
           @Override
           public void afterTextChanged(Editable s) {
