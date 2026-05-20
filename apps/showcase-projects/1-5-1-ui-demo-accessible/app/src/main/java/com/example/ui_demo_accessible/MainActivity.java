50942ae48851474faf5f8dc3fa87c7b94308768d
imromanda <imromanda@gmail.com>
2026-05-20 19:36:06 +0200

    feat: enable button from EditText and update overlay text

--- apps/showcase-projects/1-5-1-ui-demo-accessible/app/src/main/java/com/example/ui_demo_accessible/MainActivity.java
+++ apps/showcase-projects/1-5-1-ui-demo-accessible/app/src/main/java/com/example/ui_demo_accessible/MainActivity.java
@@ -1,49 +1,79 @@
 package com.example.ui_demo_accessible;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.ui_demo_accessible.databinding.ActivityMainBinding;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding; // ViewBinding para activity_main.xml
+  private ActivityMainBinding binding; // ViewBinding para activity_main.xml
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        // Inflamos el layout usando ViewBinding
+    // Inflamos el layout usando ViewBinding
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
 
-    //Listener moderno con lambda
-        binding.button1.setOnClickListener(v -> {
-            //Acción al presionar el botón: Cambiar el texto
-            binding.textTitle.setText("¡Has pulstado el boton!");
-
-
-
-
+    // Listener moderno con lambda
+    binding.button1.setOnClickListener(
+        v -> {
+          // Acción al presionar el botón: Cambiar el texto
+          binding.textTitle.setText("¡Has pulstado el boton!");
         });
 
-        //Listener segundo botón con lambda
-        binding.button2.setOnClickListener(v -> {
-            //Acción al presionar el botón: Mostrar el FrameLayout
-            binding.frameContent.setVisibility(View.VISIBLE);
-
+    // Listener segundo botón con lambda
+    binding.button2.setOnClickListener(
+        v -> {
+          // Acción al presionar el botón: Mostrar el FrameLayout
+          binding.frameContent.setVisibility(View.VISIBLE);
         });
 
-        //Listener moderno con lambda
-        binding.button3.setOnClickListener(v -> {
-            //Acción al presionar el botón: Vuelve al estado inicial
-            binding.textTitle.setText("Demo de Interfaz");
-            binding.frameContent.setVisibility(View.GONE);
-
-
+    // Listener tercer botón con lambda
+    binding.button3.setOnClickListener(
+        v -> {
+          // Acción al presionar el botón: Vuelve al estado inicial
+          binding.textTitle.setText("Demo de Interfaz");
+          binding.frameContent.setVisibility(View.GONE);
         });
 
-    }
+    // Funcionalidad para escuchar el input newMessage
+    binding.newMessage.addTextChangedListener(
+        // nuevo objeto TextWatcher que revisa cambios en el texto del input
+        /*Tiene 3 métodos obligatorios,
+        que hay que declarar aunque no se usen.
+        Aquí vamos a usar solo el afterTextChanged
+        * */
+        new TextWatcher() {
+
+          @Override
+          public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+          @Override
+          public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+          @Override
+          public void afterTextChanged(Editable s) {
+            /*Habilita el botón Refresh si s (que es el texto que se ha cambiado)
+             es convertido a string
+            le quitas lo que sobra antes y después
+            y está vacío
+            */
+            binding.btnRefresh.setEnabled(!s.toString().trim().isEmpty());
+          }
+        });
+
+    // El botón Refresh debe estar deshabilitado de primeras
+    binding.btnRefresh.setEnabled(false);
+
+    // Listener botón 4 con lambda
+    binding.btnRefresh.setOnClickListener(
+        v -> {
+          /*onClick = Cambia el texto del overlay por lo introducido en el textOverlay */
+          binding.textOverlay.setText(binding.newMessage.getText().toString());
+        });
+  }
 }
