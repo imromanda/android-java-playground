52c77587679204b7da4cd7bddb1f4648590a5006
Romanda <imromanda@gmail.com>
2026-06-01 15:27:08 +0200

    App 8 - Sensor management initial commit

--- apps/showcase-projects/8_1_Sensores/app/src/main/java/com/example/a8_1_sensores/MainActivity.java
+++ apps/showcase-projects/8_1_Sensores/app/src/main/java/com/example/a8_1_sensores/MainActivity.java
@@ -14,6 +14,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+//
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
