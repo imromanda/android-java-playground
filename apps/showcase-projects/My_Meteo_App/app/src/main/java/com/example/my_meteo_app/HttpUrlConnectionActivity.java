f9ff52b8bf65c84d4724d44a059c723938080516
imromanda <imromanda@gmail.com>
2026-06-12 17:19:31 +0200

    Refactor: extract function getIcon to be used in other activities

--- apps/showcase-projects/My_Meteo_App/app/src/main/java/com/example/my_meteo_app/HttpUrlConnectionActivity.java
+++ apps/showcase-projects/My_Meteo_App/app/src/main/java/com/example/my_meteo_app/HttpUrlConnectionActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -147,51 +146,6 @@ public class HttpUrlConnectionActivity extends AppCompatActivity {
         .start();
     }
 
-    public static int getIcon(int weatherCode, int isDay) {
 
-        boolean isDayTime = (isDay == 1);
-
-        switch (weatherCode) {
-
-            case 0:
-                return isDayTime ? R.drawable.w01d : R.drawable.w01n;
-
-            case 1:
-                return isDayTime ? R.drawable.w02d : R.drawable.w02n;
-
-            case 2:
-                return isDayTime ? R.drawable.w03d : R.drawable.w03n;
-
-            case 3:
-                return isDayTime ? R.drawable.w04d : R.drawable.w04n;
-
-            case 45:
-            case 48:
-                return isDayTime ? R.drawable.w50d : R.drawable.w50n;
-
-            case 51:
-            case 53:
-            case 55:
-                return isDayTime ? R.drawable.w09d : R.drawable.w09n;
-
-            case 61:
-            case 63:
-            case 65:
-                return isDayTime ? R.drawable.w10d : R.drawable.w10n;
-
-            case 80:
-            case 81:
-            case 82:
-                return isDayTime ? R.drawable.w09d : R.drawable.w09n;
-
-            case 95:
-            case 96:
-            case 99:
-                return isDayTime ? R.drawable.w11d : R.drawable.w11n;
-
-            default:
-                return isDayTime ? R.drawable.w01d : R.drawable.w01n;
-        }
-    }
 
         }
